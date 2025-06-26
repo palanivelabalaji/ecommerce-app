607788630a3eb5a6aa63116242f8ab4363fc80ec
palanivelabalaji <balajipalanivela@gmail.com>
2025-06-26 11:21:48 +0530

    Updated admin panel

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import {assets} from '../assets/assets'
 const Navbar = ({setToken}) => {
   return (
     <div className='flex items-center justify-between py-2 px-[4%]'>
-        <img className='w-[max(10%,80px)]' src={assets.logo} alt="" />
+        <img className='w-[max(10%,80px)]' src={assets.foreverlogo} alt="" />
         <button onClick={()=>setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
   )
